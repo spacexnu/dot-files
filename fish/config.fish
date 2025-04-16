ef10f4454850a6b28d89a3205e552be1b597cc56
Adler Medrado <adlermedrado@gmail.com>
2025-04-16 09:19:02 -0300

    update fish_config

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,7 @@ set -U fish_user_paths /opt/homebrew/sbin $fish_user_paths
 set -U fish_user_paths /Users/amedrado/.config/emacs/bin $fish_user_paths
 set -U fish_user_paths /Users/amedrado/.local/bin $fish_user_paths
 set -U fish_user_paths /usr/local/opt/mysql-client/bin $fish_user_paths
-
+set -U fish_user_paths '/Applications/IntelliJ IDEA.app/Contents/MacOS' $fish_user_paths
 # Enable colors on ls command
 set -x CLICOLOR 1
 set -x LSCOLORS GxFxCxDxBxegedabagaced
@@ -24,3 +24,15 @@ alias ls "ls -G"
 alias vim nvim
 alias ll "ls -latr --color=auto"
 alias ss "nvim (fzf -m --preview='bat --color=always {}')"
+
+test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
+
+# cat ~/.config/fish/cos_intro.txt
+source ~/.config/fish/cos_intro.fish
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /Users/spacexnu/.lmstudio/bin
