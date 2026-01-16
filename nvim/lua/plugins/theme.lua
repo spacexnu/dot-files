503d351928da8f4767cb19c21dd03f6d98cd50ab
Adler Medrado <adler@adlermedrado.com.br>
2026-01-16 09:28:18 -0300

    add rose pine moon theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -15,7 +15,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme("rose-pine-dawn")
+      vim.cmd.colorscheme("rose-pine-moon")
     end,
   },
 }
